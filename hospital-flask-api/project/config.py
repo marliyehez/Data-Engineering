d28cf4242576952fd5850617c8ee5713faad8721
marliyehez <74520448+marliyehez@users.noreply.github.com>
2023-08-26 13:16:07 +0700

    Add files via upload

---
+++ hospital-flask-api/project/config.py
@@ -0,0 +1,6 @@
+# SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite3'
+# SQLALCHEMY_DATABASE_URI =  'postgresql://postgres:password@localhost:5432/tht'
+SQLALCHEMY_DATABASE_URI = 'postgresql://myuser:mypassword@db/mydatabase'
+
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SECRET_KEY = 'this is secret'
